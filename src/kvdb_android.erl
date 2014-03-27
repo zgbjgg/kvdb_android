41d01b709e9e578fbf03d14b04d8998959073026
zgbjgg <zgbjgg@gmail.com>
2014-03-27 16:33:32 -0600

    kvdb android api for targets and module_test example

--- src/kvdb_android.erl
+++ src/kvdb_android.erl
@@ -1,6 +1,15 @@
 -module(kvdb_android).
 
--export([start/0]).
+-export([start/0,
+	 put/2,
+	 get/4,
+	 update/6,
+	 delete/2,
+	 multi_delete/1]).
+
+-define(OK, <<"ok">>).
+-define(ERROR, <<"error">>).
+-define(ERROR_NOTFOUND, <<"error_notfound">>).
 
 %%
 %% Start the entire kvdb_android application from an erlang flag
@@ -9,3 +18,70 @@
 %%
 start() ->
     application:start(?MODULE).
+
+
+%% 
+%% API for 'PUT' action over mnesia backend.
+%% This api retrieves a valid response as {ok, Reply} and must be
+%% used on each module that is set as a target. 
+%%
+put(Table, Attr) ->
+    case kvdb_android_mnesia:put(Table, Attr) of 
+        ok ->
+	    {ok, ?OK};
+	_  ->
+	    {ok, ?ERROR} 
+    end.
+
+%% 
+%% API for 'GET' action over mnesia backend.         
+%% This api retrieves a valid response as {ok, Reply} and must be
+%% used on each module that is set as a target.
+%%
+get(Table, Field, MatchField, MatchValue) ->
+    case kvdb_android_mnesia:get(Table, Field, MatchField, MatchValue) of
+        []  ->
+	    {ok, ?ERROR_NOTFOUND};
+	Obj ->
+	    {ok, Obj}
+    end.
+
+%% 
+%% API for 'UPDATE' action over mnesia backend.         
+%% This api retrieves a valid response as {ok, Reply} and must be
+%% used on each module that is set as a target.
+%%
+update(Type, Table, Field, NewValue, MatchField, MatchValue) -> 
+    case kvdb_android_mnesia:update({Type, Table, Field, NewValue, MatchField, MatchValue}) of
+        ok ->
+	    {ok, ?OK};
+	_  ->
+	    {ok, ?ERROR}
+    end.
+
+%%
+%% API for 'DELETE' action over mnesia backend.
+%% This api retrieves a valid response as {ok, Reply} and must be
+%% used on each module that is set as a target.
+%%
+delete(Table, Key) ->
+    case kvdb_android_mnesia:delete(Table, Key) of
+        ok ->
+	    {ok, ?OK};
+	_  ->
+	    {ok, ?ERROR}
+    end.
+
+%%
+%% API for 'MULTI-DELETE' action over mnesia backend.
+%% This api retrieves a valid response as {ok, Reply} and must be
+%% used on each module that is set as a target.
+%%
+multi_delete(KV) ->
+    case kvdb_android_mnesia:multi_delete(KV) of
+        ok ->
+	    {ok, ?OK};
+	_  ->
+	    {ok, ?ERROR}
+    end.
+
--- src/module_test.erl
+++ src/module_test.erl
@@ -2,10 +2,53 @@
 
 -export([catching/2]).
 
-catching(<<"GET">>, BinKey) ->
-    Key = binary_to_list(BinKey),
-    [Obj] = kvdb_android_mnesia:get(your_table, data1, yourkey, [Key]),
-    {ok, list_to_binary(Obj)};
-catching(<<"POST">>, _Body) ->
-    ok = kvdb_android_mnesia:put(your_table, [{yourkey, "Key"}, {data1, "Hello"}, {data2, "From"}, {data3, "KVDB"}]),
-    {ok, <<"ok">>}.
+%%
+%% This module is a 'TARGET' module and catch the incoming calls of the java impl.
+%% 
+%% The first parameter of the function is the type of the action and the second
+%% is the data to get working. In this case the data are set in a plain text, but you
+%% can usage a xml or json parser for a more specific and standard data protocol.
+%%
+%% The example put, get, delete and update data into a backend table set into a 
+%% file definition of tables.
+%%
+catching(<<"POST">>, PlainText) ->
+
+    % Plain text incoming are a binary string 
+    % comma separated (split it)
+    [Key, Data1, Data2, Data3] = [ binary_to_list(D) || D <- binary:split(PlainText, <<",">>, [global]) ],
+
+    % Build a plist
+    Plist = [{yourkey, Key}, {data1, Data1}, {data2, Data2}, {data3, Data3}],
+
+    % Save
+    kvdb_android:put(your_table, Plist);
+catching(<<"GET">>, PlainText) ->
+    
+    % Plain text incoming are a binary comma separated, 
+    % first is a field to return, second is a match-field and third is a match-value
+    [Field, MatchField, MatchValue] =  [ binary_to_list(D) || D <- binary:split(PlainText, <<",">>, [global]) ],
+
+    % Query
+    FieldA = list_to_atom(Field),
+    MatchFieldA = list_to_atom(MatchField),
+    case kvdb_android:get(your_table, FieldA, [MatchFieldA], [MatchValue]) of
+        {ok, [Obj]} -> {ok, list_to_binary(Obj)};
+	Error	    -> Error
+    end;	
+catching(<<"PUT">>, PlainText) ->
+    
+    % Plain text incoming are a binary comma separated, 
+    % first is a field and value to update, nex match-field and last is a match-value
+    [Field, NewValue, MatchField, MatchValue] =  [ binary_to_list(D) || D <- binary:split(PlainText, <<",">>, [global]) ],
+
+    FieldA = list_to_atom(Field),
+    MatchFieldA = list_to_atom(MatchField),
+    kvdb_android:update(replace, your_table, FieldA, NewValue, MatchFieldA, MatchValue);
+catching(<<"DEL">>, PlainText) ->
+    
+    % Plain text incoming are a binary comma separated
+    [Table, Key] =  [ binary_to_list(D) || D <- binary:split(PlainText, <<",">>, [global]) ],
+    
+    kvdb_android:delete(Table, Key).
+
